07d9ad49d04339e014b8678bc86fe1a246178f1b
Genndy <kochevgenazp@gmail.com>
2020-06-06 16:25:00 +0300

    Started some file worker class

--- oop.py
+++ oop.py
@@ -1,5 +1,22 @@
 import struct
-Head = {}
-rawData = None
 from scipy import interpolate  # interpolation function
 import numpy as np  # interpolation works only to numpy arrays
+
+class FileWorker:
+    def __init__(self, filename):
+        self.filename = filename
+        self.head = {}
+        self.raw_data = None
+
+    def get_file_properties(self):
+        with open(self.filename, 'rb') as f:
+            chunk_id = f.read(4)   # Here we get Id of chunk
+            if chunk_id != b'RIFF':  # Checking file type validity
+                print("File is not RIFF-type")
+                return None
+            
+            self.head['chunk_id'] = 'RIFF'  # Declared file chunk type
+
+
+            
+            
