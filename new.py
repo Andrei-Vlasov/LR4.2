b7596cf7f68ff3e55e6aff253b3bb871bc4f452d
Genndy <kochevgenazp@gmail.com>
2020-06-06 13:56:41 +0300

    Managed code problems and time of commits

--- new.py
+++ new.py
@@ -31,16 +31,17 @@ with open(filename, "rb") as f:
     if format != b'WAVE':
         print('not a WAV file')
         exit(1)
-    Head['Format'] = chunkSize
+    Head['Format'] = 'WAVE'
 
     while f.tell() < 8 + chunkSize:
         tag = f.read(4)
         subchunkSize = struct.unpack('<L', f.read(4))[0]
         if tag == b'fmt ':
+            Head['Chunk1ID'] = 'fmt '
             Head['Subchunk1Size'] = subchunkSize
             fmtData = f.read(subchunkSize)
             fmt, numChannels, sampleRate, byteRate, blockAlign, bitsPerSample = struct.unpack('<HHLLHH', fmtData)
-            Head['Subchunk1ID'] = fmt
+            Head['AudioFormat'] = fmt
             Head['NumChannels'] = numChannels
             Head['SampleRate'] = sampleRate
             Head['ByteRate'] = byteRate
@@ -74,9 +75,32 @@ while x < Head['Subchunk2Size']-1:
 
 new_subchunk2Size = len(new_coords)
 new_chunkSize = Head['ChunkSize'] - Head['Subchunk2Size'] + new_subchunk2Size
-
+Head['Subchunk2Size'] = new_subchunk2Size
+Head['ChunkSize'] = new_chunkSize
 graph = interpolate.interp1d(np.array(old_coords), np.array(list(rawData)))  # 3 параметр функции - квадратная, кубическая.. см. scipy interpolate
 new_samples = graph(np.array(new_coords)).tolist()  # через полученную функцию пропускаем новые переменные
+for i in range(new_subchunk2Size):
+    new_samples[i] = round(new_samples[i])
 
 
-# new_rawData = struct.pack()
+with open(filename, "rb") as f:
+    starter = f.read()[0:44]
+    print(starter)
+
+
+with open(outname, 'wb') as out:
+    for el in Head.keys():
+        print(el, Head[el])
+        if type(Head[el]) is str:
+            out.write(struct.pack('>4s', Head[el].encode('utf-8')))
+        elif el in ['AudioFormat','NumChannels','BlockAlign', 'BitsPerSample']:
+            out.write(struct.pack('<H', Head[el]))
+        else:
+            out.write(struct.pack('<I', Head[el]))
+    for sample in new_samples:
+        out.write(struct.pack('>B', sample))
+
+
+with open(outname, "rb") as f:
+    starter = f.read()[0:44]
+    print(starter)
