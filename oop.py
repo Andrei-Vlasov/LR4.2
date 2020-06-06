ff5a9fd761e465536c96213f8f50dd833af0d8ff
Genndy <kochevgenazp@gmail.com>
2020-06-06 16:35:25 +0300

    Processed some RIFF chunk descriptor

--- oop.py
+++ oop.py
@@ -1,4 +1,4 @@
-import struct
+from struct import pack, unpack
 from scipy import interpolate  # interpolation function
 import numpy as np  # interpolation works only to numpy arrays
 
@@ -17,6 +17,14 @@ class FileWorker:
             
             self.head['chunk_id'] = 'RIFF'  # Declared file chunk type
 
+            chunk_size = unpack("<L", f.read(4))[0]  # Finding chunk size
+            self.head['chunk_size'] = chunk_size  # Declared chunk size
 
+            file_format = f.read(4)
+            if file_format != b'WAVE':  # Checking file format
+                print("File is not WAV format")
+                return None
+            self.head['file_format'] = 'WAVE'  # Declaring file format
+            
             
             
