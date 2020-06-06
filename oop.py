b4e46438378af795602d74a25bd4ce7e599aa776
Genndy <kochevgenazp@gmail.com>
2020-06-06 16:56:34 +0300

    Processed fmt and data subchunks

--- oop.py
+++ oop.py
@@ -10,6 +10,7 @@ class FileWorker:
 
     def get_file_properties(self):
         with open(self.filename, 'rb') as f:
+            # Processing RIFF chunk descriptor
             chunk_id = f.read(4)   # Here we get Id of chunk
             if chunk_id != b'RIFF':  # Checking file type validity
                 print("File is not RIFF-type")
@@ -26,5 +27,28 @@ class FileWorker:
                 return None
             self.head['file_format'] = 'WAVE'  # Declaring file format
             
-            
-            
+            # Processing fmt and data subchunk
+            while f.tell() < 8 + chunk_size: # Scrolling through left file
+                tag = f.read(4)   # Checking every byte combination
+                sub_chunk_size = unpack('<L', f.read(4))[0]
+                # Checking what info contains a tag we found 
+                if tag == b'fmt ':   # 'fmt ' subchunk
+                    self.head['sub_chunk_1_id'] = 'fmt '
+                    self.head['sub_chunk_1_size'] = sub_chunk_size
+                    fmt_data = f.read(sub_chunk_size)
+                    fmt, num_channels, sample_rate, byte_rate, block_align, bits_per_sample = unpack('<HHLLHH', fmt_data)
+                    self.head['audio_format'] = fmt
+                    self.head['num_channels'] = num_channels
+                    self.head['sample_rate'] = sample_rate
+                    self.head['byte_rate'] = byte_rate
+                    self.head['block_align'] = block_align
+                    self.head['bits_per_sample'] = bits_per_sample
+                # 'data' subchunk
+                elif tag == b'data':
+                    self.head['sub_chunk_2_id'] = 'data'
+                    self.head['sub_chunk_2_size'] = sub_chunk_size
+                    self.raw_data = f.read(sub_chunk_size)
+                    break   # We have already read all data
+                # In case subchunk type not 'fmt ' or 'data'
+                else:
+                    f.seek(sub_chunk_size, 1) 
