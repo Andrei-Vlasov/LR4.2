f244969ba0bf601579ee7be7ee8f467ca16a290c
Genndy <kochevgenazp@gmail.com>
2020-06-06 14:45:20 +0300

    Added some useful comments and made some small refactoring

--- new.py
+++ new.py
@@ -20,15 +20,16 @@ q = 3.5   # float(user[2])
 
 with open(filename, "rb") as f:
     chunkId = f.read(4)
-    if chunkId != b'RIFF':
+    if chunkId != b'RIFF':  # Checking file type
         print('not a valid RIFF file')
         exit(1)
     Head['ChunkID'] = 'RIFF'
 
     chunkSize = struct.unpack('<L', f.read(4))[0]
+    # print(chunkSize)
     Head['ChunkSize'] = chunkSize
     format = f.read(4)
-    if format != b'WAVE':
+    if format != b'WAVE':    # Checking format
         print('not a WAV file')
         exit(1)
     Head['Format'] = 'WAVE'
@@ -37,7 +38,7 @@ with open(filename, "rb") as f:
         tag = f.read(4)
         subchunkSize = struct.unpack('<L', f.read(4))[0]
         if tag == b'fmt ':
-            Head['Chunk1ID'] = 'fmt '
+            Head['Subchunk1ID'] = 'fmt '
             Head['Subchunk1Size'] = subchunkSize
             fmtData = f.read(subchunkSize)
             fmt, numChannels, sampleRate, byteRate, blockAlign, bitsPerSample = struct.unpack('<HHLLHH', fmtData)
@@ -69,7 +70,7 @@ while x < Head['Subchunk2Size']:
 
 x = 0
 new_coords = []  # концы новых единичных отрезков
-while x < Head['Subchunk2Size']-1:
+while x < Head['Subchunk2Size']-1:     # Increasing amount of samples
     new_coords.append(x)
     x += (1 / q)
 
@@ -77,30 +78,33 @@ new_subchunk2Size = len(new_coords)
 new_chunkSize = Head['ChunkSize'] - Head['Subchunk2Size'] + new_subchunk2Size
 Head['Subchunk2Size'] = new_subchunk2Size
 Head['ChunkSize'] = new_chunkSize
+
+
 graph = interpolate.interp1d(np.array(old_coords), np.array(list(rawData)))  # 3 параметр функции - квадратная, кубическая.. см. scipy interpolate
 new_samples = graph(np.array(new_coords)).tolist()  # через полученную функцию пропускаем новые переменные
-for i in range(new_subchunk2Size):
+
+for i in range(new_subchunk2Size):  # Making samples whole numbers
     new_samples[i] = round(new_samples[i])
 
 
-with open(filename, "rb") as f:
-    starter = f.read()[0:44]
+with open(filename, "rb") as f:  # Printing starters
+    starter = f.read()[:44]
     print(starter)
 
 
 with open(outname, 'wb') as out:
-    for el in Head.keys():
+    for el in Head.keys():    # Checking our dictionary
         print(el, Head[el])
         if type(Head[el]) is str:
-            out.write(struct.pack('>4s', Head[el].encode('utf-8')))
-        elif el in ['AudioFormat','NumChannels','BlockAlign', 'BitsPerSample']:
-            out.write(struct.pack('<H', Head[el]))
+            out.write(struct.pack('>4s', Head[el].encode('utf-8')))    # Write encoded string
+        elif el in ['AudioFormat','NumChannels','BlockAlign', 'BitsPerSample']:   
+            out.write(struct.pack('<H', Head[el]))   # Write useful information
         else:
-            out.write(struct.pack('<I', Head[el]))
-    for sample in new_samples:
+            out.write(struct.pack('<I', Head[el]))   # Write 4-byte non-string params
+    for sample in new_samples:     # Writing music data
         out.write(struct.pack('>B', sample))
 
 
-with open(outname, "rb") as f:
-    starter = f.read()[0:44]
+with open(outname, "rb") as f:  # Printing starters
+    starter = f.read()[:44]
     print(starter)
